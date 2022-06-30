5bc0198bfd08ee1a48956a3b59e91c8d86b54982
tatamiya <tamyuji1002@gmail.com>
2022-06-30 22:46:19 +0900

    add: parse feed and generate a book list

--- src/models/books.go
+++ src/models/books.go
@@ -1,12 +1,13 @@
 package models
 
 import (
+	"github.com/mmcdole/gofeed"
 	"google.golang.org/genproto/googleapis/type/datetime"
 )
 
 type BookList struct {
 	UploadDate datetime.DateTime
-	Books      []Book
+	Books      []*Book
 }
 
 type Book struct {
@@ -23,4 +24,23 @@ type Book struct {
 	PubDate         datetime.DateTime
 	CreatedDate     datetime.DateTime
 	LastUpdatedDate datetime.DateTime
+}
+
+func NewBookListFromFeed(feed *gofeed.Feed) *BookList {
+
+	var books []*Book
+	for _, item := range feed.Items {
+		book := Book{
+			Title:      item.Title,
+			Url:        item.Link,
+			Categories: item.Categories,
+			PubDate:    item.PublishedParsed,
+		}
+		books = append(books, &book)
+	}
+
+	return &BookList{
+		UploadDate: feed.PublishedParsed,
+		Books:      books,
+	}
 }
