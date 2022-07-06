1d83213585c25a2b174055a984ab0b54c438fca6
tatamiya <tamyuji1002@gmail.com>
2022-07-06 22:38:31 +0900

    change struct name

--- src/subject/decoder.go
+++ src/subject/decoder.go
@@ -1,6 +1,6 @@
 package subject
 
-type CCodeTable struct {
+type SubjectDecoder struct {
 	Taishou map[string]string `json:"taishou"`
 	Keitai  map[string]string `json:"keitai"`
 	Naiyou  map[string]string `json:"naiyou"`
