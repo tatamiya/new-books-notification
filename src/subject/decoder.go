56bd2aa9fc13a10c68634dbdae1e2a173a20b340
tatamiya <tamyuji1002@gmail.com>
2022-07-07 22:52:21 +0900

    add subject decoder

--- src/subject/decoder.go
+++ src/subject/decoder.go
@@ -1,7 +1,37 @@
 package subject
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type SubjectDecoder struct {
 	Taishou map[string]string `json:"taishou"`
 	Keitai  map[string]string `json:"keitai"`
 	Naiyou  map[string]string `json:"naiyou"`
+}
+
+type DecodedSubject struct {
+	Ccode  string
+	Target string
+	Format string
+	Genre  string
+}
+
+func (s *SubjectDecoder) decode(ccode string) (*DecodedSubject, error) {
+
+	if _, err := strconv.Atoi(ccode); err != nil {
+		return nil, fmt.Errorf("Invalid Ccode! %s cannot be converted to digits: %s", ccode, err)
+	}
+	if len(ccode) != 4 {
+		return nil, fmt.Errorf("Invalid Ccode! %s is not 4 digits", ccode)
+	}
+	chars := []rune(ccode)
+	target := s.Taishou[string(chars[0])]
+	format := s.Keitai[string(chars[1])]
+	genre := s.Naiyou[string(chars[2:])]
+
+	return &DecodedSubject{
+		ccode, target, format, genre,
+	}, nil
 }
---
+++ src/subject/decoder_test.go
@@ -0,0 +1,65 @@
+package subject
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testDecoder = SubjectDecoder{
+	Taishou: map[string]string{
+		"0": "一般",
+		"1": "教養",
+		"3": "専門書",
+	},
+	Keitai: map[string]string{
+		"0": "単行本",
+		"1": "文庫",
+	},
+	Naiyou: map[string]string{
+		"40": "自然科学総記",
+		"42": "物理学",
+	},
+}
+
+func TestDecodeSubjectCorrectly(t *testing.T) {
+
+	expectedDecoded := DecodedSubject{
+		Ccode:  "0040",
+		Target: "一般",
+		Format: "単行本",
+		Genre:  "自然科学総記",
+	}
+
+	actualDecoded, err := testDecoder.decode("0040")
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedDecoded, *actualDecoded)
+}
+
+func TestDecodingFailsWhenCcodeIsNotFourDigits(t *testing.T) {
+
+	actualDecoded, err := testDecoder.decode("040")
+	assert.NotNil(t, err)
+	assert.Nil(t, actualDecoded)
+}
+
+func TestDecodingFailsWhenCcodeContainsNonDigit(t *testing.T) {
+
+	actualDecoded, err := testDecoder.decode("1a40")
+	assert.NotNil(t, err)
+	assert.Nil(t, actualDecoded)
+}
+
+func TestDecodedResultIsEmptyWhenNotFound(t *testing.T) {
+
+	expectedDecoded := DecodedSubject{
+		Ccode:  "0049",
+		Target: "一般",
+		Format: "単行本",
+		Genre:  "",
+	}
+
+	actualDecoded, err := testDecoder.decode("0049")
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedDecoded, *actualDecoded)
+}
