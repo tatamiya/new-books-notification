819151a20a382b02016b4d66f96d3c925423a7f9
tatamiya <tamyuji1002@gmail.com>
2022-07-15 22:52:06 +0900

    add: test for favorite categories

--- src/filter/isfavorite_test.go
+++ src/filter/isfavorite_test.go
@@ -27,3 +27,35 @@ func TestReturnTrueForFavoriteCategory(t *testing.T) {
 	assert.Equal(t, true, isfav)
 
 }
+
+func TestReturnFalseForNotFavoriteCategory(t *testing.T) {
+
+	sampleBook := models.Book{
+		Categories: []string{"趣味・実用"},
+		Ccode:      "1042",
+		Target:     "教養",
+		Format:     "単行本",
+		Genre:      "物理学",
+	}
+
+	isfav := sampleFavoriteFilter.IsFavorite(&sampleBook)
+
+	assert.Equal(t, false, isfav)
+
+}
+
+func TestReturnFalseForEmptyCategory(t *testing.T) {
+
+	sampleBook := models.Book{
+		Categories: []string{},
+		Ccode:      "1042",
+		Target:     "教養",
+		Format:     "単行本",
+		Genre:      "物理学",
+	}
+
+	isfav := sampleFavoriteFilter.IsFavorite(&sampleBook)
+
+	assert.Equal(t, false, isfav)
+
+}
