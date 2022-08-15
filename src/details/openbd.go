a589ca9efee4987f73533efced00b05d9f6289c9
tatamiya <tamyuji1002@gmail.com>
2022-08-15 14:51:42 +0900

    clean: merge files related to OpenBD

--- src/details/openbd.go
+++ src/details/openbd.go
@@ -1,5 +1,11 @@
 package details
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type OpenBDResponse struct {
 	Onix    Onix    `json:"onix"`
 	Hanmoto Hanmoto `json:"hanmoto"`
@@ -34,4 +40,26 @@ type Summary struct {
 	Publisher string `json:"publisher"`
 	PubDate   string `json:"pubdate"`
 	Author    string `json:"author"`
+}
+
+type openBDClient struct {
+}
+
+func (c *openBDClient) get(isbn string) (*OpenBDResponse, error) {
+	openbdUrl := fmt.Sprintf("https://api.openbd.jp/v1/get?isbn=%s&pretty", isbn)
+	resp, respErr := http.Get(openbdUrl)
+	if respErr != nil {
+		err := fmt.Errorf("OpenBD request failed: %s", respErr)
+		return nil, err
+	}
+
+	var openBDResp []*OpenBDResponse
+	decodeErr := json.NewDecoder(resp.Body).Decode(&openBDResp)
+	if decodeErr != nil {
+		err := fmt.Errorf("error in decoding OpenBD JSON response: %s", decodeErr)
+
+		return nil, err
+	}
+
+	return openBDResp[0], nil
 }
